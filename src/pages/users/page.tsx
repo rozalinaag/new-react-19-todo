ea656aa1e217aaa1df01675212d658a0ccf46b6e
rozalinaag <59352861+rozalinaag@users.noreply.github.com>
2025-02-12 17:39:23 -0500

    userPage styles

--- src/pages/users/page.tsx
+++ src/pages/users/page.tsx
@@ -7,13 +7,34 @@ type User = {
 
 export function UsersPage() {
   const [users, setUsers] = useState<User[]>([]);
+  const [email, setEmail] = useState('');
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setUsers([...users, { id: Date.now().toString(), email }]);
+    setEmail('');
+  };
 
   return (
-    <main>
-      <h1 className="text-3xl font-bold underline">Users</h1>
+    <main className="p-10">
+      <h1 className="text-3xl font-bold underline mb-10">Users</h1>
 
       <section>
-        <form></form>
+        <form className="flex gap-2" onSubmit={handleSubmit}>
+          <input
+            type="email"
+            className="border p-2 rounded"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          />
+
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            type="submit"
+          >
+            Add
+          </button>
+        </form>
       </section>
 
       <section>
