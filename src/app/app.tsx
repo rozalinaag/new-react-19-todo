083299b17cfdfd6f256218dadb97f2c2566dfaa7
rozalinaag <59352861+rozalinaag@users.noreply.github.com>
2025-02-12 17:31:22 -0500

    page

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -5,7 +5,7 @@ import { TodoListPage } from '../pages/todo-list';
 export function App() {
   return (
     <Routes>
-      <Route path="/users" element={<UsersPage />} />
+      <Route path="/" element={<UsersPage />} />
       <Route path="/todo" element={<TodoListPage />} />
     </Routes>
   );
--- src/pages/users/page.tsx
+++ src/pages/users/page.tsx
@@ -1,3 +1,30 @@
+import { useState } from 'react';
+
+type User = {
+  id: string;
+  email: string;
+};
+
 export function UsersPage() {
-  return <h1>dd</h1>;
+  const [users, setUsers] = useState<User[]>([]);
+
+  return (
+    <main>
+      <h1 className="text-3xl font-bold underline">Users</h1>
+
+      <section>
+        <form></form>
+      </section>
+
+      <section>
+        <div className="flex flex-col">
+          {users.map((user) => (
+            <div className="border p-2 m-2 rounded bg-gray-100" key={user.id}>
+              {user.email}
+            </div>
+          ))}
+        </div>
+      </section>
+    </main>
+  );
 }
